045834de936603ebd8b3941cfcdc926d3d56a5be
sdeng238 <shirleyd2023@student.cis.edu.hk>
2023-03-03 14:35:01 +0800

    done & tested!

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,58 @@
-public class Song {
+import java.util.ArrayList;
 
-    private String
+public class Song {
+    private String title;
+    private String artist;
+    private ArrayList<String> existingListeners = new ArrayList<>();
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    //outputs how many new listeners the song gained on that day out of all the listeners
+    public int howMany(ArrayList<String> dailyListeners)
+    {
+        int count = 0;
+        boolean exists = false;
+
+        if (existingListeners.size() == 0)
+        {
+            for(int num = 0; num < dailyListeners.size(); num++)
+            {
+                count++;
+                existingListeners.add(dailyListeners.get(num).toLowerCase());
+            }
+        }
+        else
+        {
+            for(int num = 0; num < dailyListeners.size(); num++)
+            {
+                for(int numTwo = 0; numTwo < existingListeners.size(); numTwo++)
+                {
+                    if(dailyListeners.get(num).toLowerCase().equals(existingListeners.get(numTwo)))
+                    {
+                        exists = true;
+                    }
+                }
+
+                if(!exists)
+                {
+                    count++;
+                    existingListeners.add(dailyListeners.get(num).toLowerCase());
+                }
+                exists = false;
+            }
+        }
+
+        return count;
+    }
 }
